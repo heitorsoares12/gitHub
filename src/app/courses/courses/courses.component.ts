7da91f8d0aebd1bb427f8135e0d59f94b4715250
Heitor Soares <heitor.soaress@outlook.com>
2023-06-27 22:32:30 -0300

    iniciando integracao do service

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,22 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'Front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() { 
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
+    
   }
-
 }
