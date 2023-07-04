7d1783b61e03f3693a48ee8cdd4b525eb64f00b6
Heitor <hsoares.ekan@iamspe.sp.gov.br>
2023-07-04 14:34:24 -0300

    iniciando teste com cypress

--- src/app/repositorios/repositorios/repositorios.component.spec.ts
+++ src/app/repositorios/repositorios/repositorios.component.spec.ts
@@ -15,7 +15,7 @@ describe('RepositoriosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
